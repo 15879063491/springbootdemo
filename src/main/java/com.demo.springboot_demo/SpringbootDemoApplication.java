0caba3085d5d8d213451115eb499157a1a79a021
luoan <2980748519@qq.com>
2020-08-10 08:54:51 +0800

    简单测试

---
+++ src/main/java/com.demo.springboot_demo/SpringbootDemoApplication.java
@@ -0,0 +1,26 @@
+package com.demo.springboot_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+@SpringBootApplication
+public class SpringbootDemoApplication  extends SpringBootServletInitializer {
+
+
+    // 重写 configure方法
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(SpringbootDemoApplication.class);
+    }
+
+        public static void main(String[] args) {
+            SpringApplication.run(SpringbootDemoApplication.class,args);
+        }
+
+    }
+
+

034cdb8e021ba59270cff8030239fdce246eb5cd
luoan <2980748519@qq.com>
2020-08-28 10:25:02 +0800

    引入日志

--- src/main/java/com.demo.springboot_demo/controller/loginController.java
+++ src/main/java/com.demo.springboot_demo/controller/loginController.java
@@ -1,16 +1,24 @@
 package com.demo.springboot_demo.controller;
 
+
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
 
 @Controller
 public class loginController {
 
+    Logger log = Logger.getLogger(loginController.class);
 
     @RequestMapping("/login")
     public String login(){
         System.out.println("===============");
-
+        log.info("测试");
         return "login";
     }
+
+
+
 }

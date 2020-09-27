f467675f59a7df969925570346d6bca650bf1d56
luoan <2980748519@qq.com>
2020-09-27 22:21:08 +0800

    努力过好每一天

--- src/main/java/com.demo.springboot_demo/controller/loginController.java
+++ src/main/java/com.demo.springboot_demo/controller/loginController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
-
 @Slf4j
 @Controller
 public class loginController {
@@ -15,11 +14,10 @@ public class loginController {
     @RequestMapping("/login")
     public String login(){
         System.out.println("===============");
-        String a=null;
-        a.toString();
         log.info("查询节点【{}】返回报文：{}", LogWriteUtils.getJsonStr("再见了我的大学"), LogWriteUtils.getJsonStr("再见了我的应科"));
         return "login";
     }
+
 
 
 
